e3bf755d94b404062643113c07588805fc8d9d82
MahmoudAlmodalal <132272679+MahmoudAlmodalal@users.noreply.github.com>
2026-01-24 17:43:02 +0200

    Update settings.py

--- eduTrack/settings.py
+++ eduTrack/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -160,10 +162,9 @@ if 'test' in sys.argv:
         }
     }
 else:
-    import os
-
-DATABASES = {
-    'default': {
+    
+    DATABASES = {
+        'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
